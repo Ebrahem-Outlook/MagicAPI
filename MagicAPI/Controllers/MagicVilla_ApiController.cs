d083bdb2fe218754d1ec9cddc997055fb0418927
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-01-23 22:24:03 +0200

    E_1

---
+++ MagicAPI/Controllers/MagicVilla_ApiController.cs
@@ -0,0 +1,19 @@
+﻿using MagicAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicAPI.Controllers
+{
+    [Route("api/MagicVilla")]
+    [ApiController]
+    public class MagicVilla_ApiController : ControllerBase
+    {
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+                new Villa { Id=1, Name="Pool View"},
+                new Villa { Id=2, Name="Beach View"}
+
+            };
+        }
+    }
+}
